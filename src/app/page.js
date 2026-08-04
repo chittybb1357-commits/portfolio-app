9b40eea950fa1ea38fd1e97184696e289d586c75
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-04 11:26:38 +0900

    Supabase 클라이언트 경로 수정 및 변수명 오타 수정

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@/utils/supabase/client";
 
 export default async function Page() {
   const supabase = await createClient();
-  const { data: porject, error } = await supabase.from("portfolio").select();
+  const { data: projects, error } = await supabase.from("portfolio").select();
 
-  console.log(porjects);
+  console.log(projects);
 
   if (error) {
     console.error("연결실패", error);
+
     return <div>프로젝트 로드 실패</div>;
   }
 
-  return (
-    <>
-      <h1></h1>
-    </>
-  );
+  return <></>;
 }
