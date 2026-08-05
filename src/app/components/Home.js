87239a96a991ccf43bafed555eb8a698502c28d7
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-06 08:55:44 +0900

    불필요한 공백 제거 및 코드 정리

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -9,20 +9,17 @@ export default function Home({ data }) {
             <h2 className="heading2">I’m alikerock</h2>
           </div>
         </div>
-
         <div className="col-md-4">
           <div className="contents shadow">
             <h2 className="heading2">I create super awesome stuff</h2>
           </div>
         </div>
-
         <div className="col-md-4">
           <div className="contents shadow">
             <h2 className="heading2">I’m available for freelance projects</h2>
           </div>
         </div>
       </div>
-
       <div className="row list">
         {data.map(item => (
           <div className="col-md-4" key={item.id}>
@@ -32,7 +29,6 @@ export default function Home({ data }) {
                 <div className="list_info">
                   <h3>
                     <a href={`/portfolio/${item.id}`}>{item.title}</a>
-
                     <Image
                       src="/images/portfolio_list_arrow.png"
                       width={6}
@@ -40,7 +36,6 @@ export default function Home({ data }) {
                       alt="list arrow"
                     />
                   </h3>
-
                   <p>
                     <a href="">Click to see project</a>
                   </p>
@@ -50,7 +45,6 @@ export default function Home({ data }) {
           </div>
         ))}
       </div>
-
       <p className="porfolio_readmore">
         <a href="" className="primary-btn">
           See my full portfolio
