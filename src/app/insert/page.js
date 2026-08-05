a3ca8216e35131e7a5f7809fa829dad4b4b48274
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-06 08:56:08 +0900

    코드 정리 및 불필요한 공백 제거

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 export default function Insert() {
   const supabase = createClient();
   const router = useRouter();
+
   const [formData, setFormData] = useState({
     title: "",
     content: "",
@@ -20,11 +21,11 @@ export default function Insert() {
     thumbnail: "",
   });
 
+  const [thumbnail, setThumbnail] = useState(null);
+
   async function insertData(e) {
     e.preventDefault();
-
     const { error } = await supabase.from("portfolio").insert(formData);
-
     if (error) {
       console.log(error);
     } else {
@@ -32,7 +33,6 @@ export default function Insert() {
       router.push("/");
     }
   }
-
   const handleChange = e => {
     const { name, value } = e.target;
 
@@ -41,22 +41,17 @@ export default function Insert() {
       [name]: value,
     });
   };
-
   const handleFileChange = e => {
-    const [thumbnail, setThumbnail] = useState(null);
-
-    console.log(e.target.files[0]);
+    setThumbnail(e.target.files[0]);
   };
 
   return (
     <div className="about_content shadow">
       <h2 className="mb-3">데이터 입력</h2>
-
       <div className="contact_form">
         <form onSubmit={insertData}>
           <p className="field">
             <label htmlFor="title">프로젝트 이름:</label>
-
             <input
               type="text"
               id="title"
@@ -66,10 +61,8 @@ export default function Insert() {
               onChange={handleChange}
             />
           </p>
-
           <p className="field">
             <label htmlFor="content">프로젝트 설명:</label>
-
             <textarea
               name="content"
               id="content"
@@ -80,10 +73,8 @@ export default function Insert() {
               onChange={handleChange}
             ></textarea>
           </p>
-
           <p className="field">
             <label htmlFor="url">프로젝트 주소:</label>
-
             <input
               type="url"
               id="url"
@@ -92,10 +83,8 @@ export default function Insert() {
               onChange={handleChange}
             />
           </p>
-
           <p className="field">
             <label htmlFor="review">프로젝트 후기:</label>
-
             <textarea
               name="review"
               id="review"
@@ -105,10 +94,8 @@ export default function Insert() {
               onChange={handleChange}
             ></textarea>
           </p>
-
           <p className="field">
             <label htmlFor="reviewer">후기 글쓴이:</label>
-
             <input
               type="text"
               id="reviewer"
@@ -117,54 +104,24 @@ export default function Insert() {
               onChange={handleChange}
             />
           </p>
-
           <p className="field">
             <label htmlFor="rep1_img">대표 이미지 1:</label>
-
-            <input
-              type="fiile"
-              id="rep1_img"
-              name="rep1_img"
-              accept="image/*"
-            />
+            <input type="fiile" id="rep1_img" name="rep1_img" accept="image/*" />
           </p>
-
           <p className="field">
             <label htmlFor="rep1_desc">대표 이미지 1 설명</label>
-
-            <input
-              type="text"
-              id="rep1_desc"
-              name="rep1_desc"
-              onChange={handleChange}
-            />
+            <input type="text" id="rep1_desc" name="rep1_desc" onChange={handleChange} />
           </p>
-
           <p className="field">
             <label htmlFor="rep2_img">대표 이미지 2:</label>
-
-            <input
-              type="fiile"
-              id="rep2_img"
-              name="rep2_img"
-              accept="image/*"
-            />
+            <input type="fiile" id="rep2_img" name="rep2_img" accept="image/*" />
           </p>
-
           <p className="field">
             <label htmlFor="rep2_desc">대표 이미지 2 설명</label>
-
-            <input
-              type="text"
-              id="rep2_desc"
-              name="rep2_desc"
-              onChange={handleChange}
-            />
+            <input type="text" id="rep2_desc" name="rep2_desc" onChange={handleChange} />
           </p>
-
           <p className="field">
             <label htmlFor="thumbnail">썸네일:</label>
-
             <input
               type="file"
               id="thumbnail"
@@ -173,7 +130,6 @@ export default function Insert() {
               onChange={handleFileChange}
             />
           </p>
-
           <p className="submit">
             <input type="submit" className="primary-btn" value="등록" />
           </p>
