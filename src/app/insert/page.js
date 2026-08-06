d55083e158086a8e02087f648900879ffd332dca
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-06 09:53:58 +0900

    관리자 로그인 폼 추가 및 사용자 인증 체크 구현

--- src/app/insert/page.js
+++ src/app/insert/page.js
@@ -74,6 +74,40 @@ export default function Insert() {
       console.log("파일 업로드 성공:");
     }
   }
+  if (!user) {
+    return (
+      <div className="about_content shadow">
+        <h2 className="mb-3">관리자 로그인</h2>
+        <div className="contact_form">
+          <form action="">
+            <p className="field">
+              <label htmlFor="email">이메일</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email"
+                required
+              />
+            </p>
+            <p className="field">
+              <label htmlFor="password">비밀번호</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="비밀번호"
+                required
+              />
+            </p>
+            <p className="submit">
+              <input type="submit" className="primary-btn" value="로그인" />
+            </p>
+          </form>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="about_content shadow">
