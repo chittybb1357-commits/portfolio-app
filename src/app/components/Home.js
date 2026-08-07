e3ed3c51bc87cc6a7289e39315eaec5f40700ebb
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-07 10:45:33 +0900

    홈 컴포넌트 개선: Supabase 클라이언트 추가 및 공용 URL 가져오기 함수 구현

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import { createClient } from "@/utils/supabase/client";
 
 export default function Home({ data }) {
+  const supabase = createClient();
+
+  const getPublicURL = path => {
+    if (!path) return "";
+    const { data: publicUrlData } = supabase.storage
+      .from("portfolio")
+      .getPublicUrl(path);
+    return publicUrlData.publicUrl;
+  };
+
   return (
     <div className="latest_portfolio">
       <div className="row intro">
@@ -9,22 +20,11 @@ export default function Home({ data }) {
             <h2 className="heading2">I’m alikerock</h2>
           </div>
         </div>
-        <div className="col-md-4">
-          <div className="contents shadow">
-            <h2 className="heading2">I create super awesome stuff</h2>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="contents shadow">
-            <h2 className="heading2">I’m available for freelance projects</h2>
-          </div>
-        </div>
       </div>
       <div className="row list">
         {data.map(item => (
           <div className="col-md-4" key={item.id}>
             <div className="contents shadow">
-              {/* <img src="images/latest_portfolio_01.jpg" alt="latest_portfolio_01"/> */}
               <div className="hover_contents">
                 <div className="list_info">
                   <h3>
