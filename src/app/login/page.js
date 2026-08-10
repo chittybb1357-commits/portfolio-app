d4b8c1f3142a374155eae3df48e31a5600dc8093
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-10 13:43:13 +0900

    코드 수정: 로그인 기능 개선 및 오류 메시지 추가

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 
 export default function Login() {
   const supabase = createClient();
+
   const router = useRouter();
 
   const [authForm, setAuthform] = useState({
@@ -14,21 +15,27 @@ export default function Login() {
 
   const handleAuthChange = e => {
     const { name, value } = e.target;
+
     setAuthform(prev => ({ ...prev, [name]: value }));
   };
 
-  //로그인 진행
+  // 로그인 진행
   const handleLogin = async e => {
     e.preventDefault();
+
     const { data, error } = await supabase.auth.signInWithPassword(authForm);
+
     if (error) {
       alert("로그인 실패", error.message);
     } else {
       alert("로그인 성공");
+
       router.push("/");
+
       router.refresh();
     }
   };
+
   const signInWithGoogle = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
@@ -36,10 +43,12 @@ export default function Login() {
         redirectTo: window.location.origin,
       },
     });
+
     if (error) {
       console.error("로그인 실패", error.message);
     }
   };
+
   const signInWithKakao = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "kakao",
@@ -47,17 +56,21 @@ export default function Login() {
         redirectTo: window.location.origin,
       },
     });
+
     if (error) {
       console.error("로그인 실패", error.message);
     }
   };
+
   return (
     <div className="about_content shadow">
       <h2>로그인</h2>
+
       <div className="contact_form">
         <form onSubmit={handleLogin}>
           <p className="field">
             <label htmlFor="email">이메일</label>
+
             <input
               type="email"
               id="email"
@@ -67,8 +80,10 @@ export default function Login() {
               onChange={handleAuthChange}
             />
           </p>
+
           <p className="field">
             <label htmlFor="password">비밀번호</label>
+
             <input
               type="password"
               id="password"
@@ -78,12 +93,16 @@ export default function Login() {
               onChange={handleAuthChange}
             />
           </p>
+
           <p className="submit">
             <input type="submit" className="primary-btn" value="로그인" />
           </p>
         </form>
+
         <hr />
+
         <button onClick={signInWithGoogle}>구글로 로그인</button>
+
         <button onClick={signInWithKakao}>카카오로 로그인</button>
       </div>
     </div>
