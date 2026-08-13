831f7d22241b9f64463bf8f6cbc90dfc1f086358
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-13 10:25:54 +0900

    코드 추가: 사이트맵 기능 구현 및 기본 URL 목록 반환

---
+++ src/app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://abc.com/",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://abc.com/about",
+      lastModified: new Date(),
+    },
+  ];
+}
