65060ee62b8c94d4222f573fa73a538ba1617a4b
chittybb1357-commits <chittybb1357@gmail.com>
2026-08-13 10:06:23 +0900

    코드 수정: About 페이지 메타데이터 추가 및 텍스트 정렬 개선

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,16 +1,33 @@
+export const metadata = {
+  title: "About - Minimal portfolio",
+  description: "The Most Fantastic and Flawless Portfolio in the World",
+  openGraph: {
+    title: "React 강의",
+    description: "React 실전 강의",
+    images: [
+      {
+        url: "/thumb.jpg",
+      },
+    ],
+  },
+};
+
 export default function About() {
   return (
     <div className="about_content shadow">
       <h3 className="heading6">About Me</h3>
       <p className="narrow-p">
-        I&apos;m a web designer based in Romania. I create clean websites, love Apple products and
-        I&apos;m a big fan of trance music.
+        I&apos;m a web designer based in Romania. I create clean websites, love
+        Apple products and I&apos;m a big fan of trance music.
       </p>
       <p className="narrow-p">
-        Wanna get in touch? Do a quick scroll to the bottom of the page. It&apos;s all there :)
+        Wanna get in touch? Do a quick scroll to the bottom of the page.
+        It&apos;s all there :)
       </p>
       <hr className="double" />
-      <h4 className="heading4 available">I&apos;m currently available for freelance projects.</h4>
+      <h4 className="heading4 available">
+        I&apos;m currently available for freelance projects.
+      </h4>
       <h4 className="heading4 rate">Rates start at $50/hour.</h4>
       <p className="portfolio_link">
         <a href="" className="primary-btn">
